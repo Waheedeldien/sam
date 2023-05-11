4642a12639d7c146744b37b644a685be22322d09
Waheedeldien <waheedeldien95@gmail.com>
2023-05-11 15:37:28 +0200

    sam

--- main.py
+++ main.py
@@ -4,12 +4,12 @@ import sys
 # sys.path.append("..")
 from segment_anything import SamAutomaticMaskGenerator, sam_model_registry, SamPredictor
 
-
+img= cv2.imread("/sam/sam/input/sincerely-media-VDPauwJ_sHo-unsplash.jpg")
 model_type = "vit_h"
 
 
 sam = sam_model_registry[model_type](checkpoint="sam_vit_h_4b8939.pth")
 mask_generator = SamAutomaticMaskGenerator(sam)
-masks = mask_generator.generate("/sam/sam/input/sincerely-media-VDPauwJ_sHo-unsplash.jpg")
+masks = mask_generator.generate(img)
 
 print(masks)
