103c44c1ac4a714677c3edb2e2213a656f99eae1
Waheedeldien <waheedeldien95@gmail.com>
2023-05-11 15:35:10 +0200

    sam

--- main.py
+++ main.py
@@ -11,4 +11,5 @@ model_type = "vit_h"
 sam = sam_model_registry[model_type](checkpoint="sam_vit_h_4b8939.pth")
 mask_generator = SamAutomaticMaskGenerator(sam)
 masks = mask_generator.generate("/sam/sam/input/sincerely-media-VDPauwJ_sHo-unsplash.jpg")
+
 print(masks)
