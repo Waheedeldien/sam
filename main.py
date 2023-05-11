d233218da58eac72716c84bec49c63519b2e3968
Waheedeldien <waheedeldien95@gmail.com>
2023-05-11 15:28:02 +0200

    sam

--- main.py
+++ main.py
@@ -1,15 +1,14 @@
 import cv2
 import os
 import sys
-sys.path.append("..")
+# sys.path.append("..")
 from segment_anything import SamAutomaticMaskGenerator, sam_model_registry, SamPredictor
 
 
+model_type = "vit_h"
 
 
-
-
-
-sam = sam_model_registry["<model_type>"](checkpoint="<path/to/checkpoint>")
+sam = sam_model_registry[model_type](checkpoint="sam_vit_h_4b8939.pth")
 mask_generator = SamAutomaticMaskGenerator(sam)
-masks = mask_generator.generate(input/sincerely-media-VDPauwJ_sHo-unsplash.jpg)
+masks = mask_generator.generate("input/sincerely-media-VDPauwJ_sHo-unsplash.jpg")
+print(masks)
