fdd2ee79b346653109a2191effe4b9292fe16d54
Waheedeldien <waheedeldien95@gmail.com>
2023-05-11 15:43:50 +0200

    sam

--- main.py
+++ main.py
@@ -7,8 +7,11 @@ from segment_anything import SamAutomaticMaskGenerator, sam_model_registry, SamP
 img= cv2.imread("input/sincerely-media-VDPauwJ_sHo-unsplash.jpg")
 model_type = "vit_h"
 
+device = "cuda"
 
 sam = sam_model_registry[model_type](checkpoint="sam_vit_h_4b8939.pth")
+sam.to(device=device)
+
 mask_generator = SamAutomaticMaskGenerator(sam)
 masks = mask_generator.generate(img)
 
